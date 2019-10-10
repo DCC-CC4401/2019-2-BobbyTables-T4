478a344efd5dc42a9fccc57dd8d642bcee80fa49
Joaquin Larrain <joaquin.larrain@ug.uchile.cl>
2019-10-10 14:43:50 -0300

    se termino la parte de Actividades, se agrego get y set a todas las clases.

--- BTT4App/models.py
+++ BTT4App/models.py
@@ -28,10 +28,23 @@ class PersonaNatural(models.Model):
         user = User(first_name=primerNombre, last_name=apellido, email=email, password=password)
         user.save()
 
-        personaNatural = PersonaNatural(user=user)
-        personaNatural.save()
+        persona_natural = PersonaNatural(user=user)
+        persona_natural.save()
 
-        return personaNatural
+        return persona_natural
+
+    def get_nombre(self):
+        return self.user.first_name
+
+    def get_apellido(self):
+        return self.user.last_name
+
+    def get_email(self):
+        return self.user.email
+
+    def get_password(self):
+        return self.user.password
+
 
 
 class Administrador(models.Model):
@@ -50,6 +63,12 @@ class Administrador(models.Model):
 
         return admin
 
+    def get_email(self):
+        return self.user.email
+
+    def get_password(self):
+        return self.user.password
+
 
 class IActividad(models.Model):
     nombre = models.CharField(max_length=100, blank=False)
@@ -59,28 +78,92 @@ class IActividad(models.Model):
     def create_iActividad(self, nombre, descripcion, categoria):
         actividad = IActividad(nombre=nombre, descripcion=descripcion, categoria=categoria)
         actividad.save()
+
         return actividad
+
+    def get_nombre(self):
+        return self.nombre
+
+    def get_descripcion(self):
+        return self.descripcion
+
+    def get_categoria(self):
+        return self.categoria
 
 
 class ActividadTipo(IActividad):
-    persona = models.ForeignKey(PersonaNatural, models.SET_NULL, null=True, blank=False)
+    fechaYHora = models.DateTimeField(default='Sin fecha', blank=True)
+    duracion = models.IntegerField(default='Sin duracion', blank=True)
+    persona = models.ForeignKey(PersonaNatural, models.SET_NULL, null=True, blank=True)
+
+    def create_ActividadTipo(self, nombre, descripcion, categoria):
+        actividad_tipo = super(nombre, descripcion, categoria)
+        actividad_tipo.save()
+
+        return actividad_tipo
+
+    def set_fecha_y_hora(self, fechaYHora):
+        self.fechaYHora = fechaYHora
+
+    def set_duracion(self, duracion):
+        self.duracion = duracion
+
+    def set_persona(self, persona):
+        self.persona = persona
+
+    def get_fechaYHora(self):
+        return self.fechaYHora
+
+    def get_duracion(self):
+        return self.duracion
+
+    def get_persona(self):
+        return self.persona
 
 
 class AbsActividad(IActividad):
-    fechaYHora = models.DateTimeField(default='Sin fecha')
-    duracion = models.IntegerField(default='Sin duracion')
+    fechaYHora = models.DateTimeField(default='Sin fecha', blank=False)
+    persona = models.ForeignKey(PersonaNatural, models.SET_NULL, null=True, blank=False)
 
-    persona = models.ForeignKey(PersonaNatural, models.SET_NULL, null=True, blank=True)
+    def create_absActividad(self, nombre, descripcion, categoria, fechaYHora, persona):
+        abs_actividad = IActividad(fechaYHora=fechaYHora, persona=persona)
+        super(nombre, descripcion, categoria)
+        abs_actividad.save()
 
-    def create_absActividad(self, fechaYHora, duracion):
-        actividad = IActividad(fechaYHora=fechaYHora, duracion=duracion)
-        actividad.save()
-        return actividad
+        return abs_actividad
+
+    def get_fechaYHora(self):
+        return self.fechaYHora
+
+    def get_persona(self):
+        return self.persona
 
 
 class ActividadTiempoReal(AbsActividad):
-    pass
+    duracion = models.IntegerField(default='Sin duracion', blank=True)
+
+    def create_ActividadTiempoReal(self, nombre, descripcion, categoria, fechaYHora, persona):
+        actividad_tiempo_real = super(nombre, descripcion, categoria, fechaYHora, persona)
+        actividad_tiempo_real.save()
+
+        return actividad_tiempo_real
+
+    def set_duracion(self, duracion):
+        self.duracion = duracion
+
+    def get_duracion(self):
+        return self.duracion
 
 
 class ActividadAPosteriori(AbsActividad):
-    pass
+    duracion = models.IntegerField(default='Sin duracion', blank=False)
+
+    def create_ActividadAPosteriori(self, nombre, descripcion, categoria, fechaYHora, duracion, persona):
+        actividad_tipo = ActividadTipo(duracion=duracion)
+        super(nombre, descripcion, categoria, fechaYHora, persona)
+        actividad_tipo.save()
+
+        return actividad_tipo
+
+    def get_duracion(self):
+        return self.duracion
