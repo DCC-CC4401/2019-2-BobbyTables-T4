e083a5b337af6fa59f9494d8ca2adb519bdfcf10
cmesiasd <cmesiasd@gmail.com>
2019-10-16 18:56:54 -0300

    cambios pequeños en templates

--- BTT4App/urls.py
+++ BTT4App/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 urlpatterns = [
     path('', landingPage, name='landingPage'),
     path('profile/', profile, name='profile'),
-    path('login/', login, name='login')
+    path('login/', login, name='login'),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
