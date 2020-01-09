710d8396cfea848f517ff2d559294ee8375e92ea
Joaquin Larrain <joaquin.larrain@ug.uchile.cl>
2020-01-09 16:37:58 -0300

    eh 2

--- BTT4App/models.py
+++ BTT4App/models.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import User
 
 class PersonaNatural(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    fotoDePerfil = models.ImageField(blank=True, null=True)
+    fotoDePerfil = models.ImageField(blank=True, default='profile_pics/turing.jpg', null=True, upload_to='profile_pics')
+
     amigos = models.ManyToManyField('self', blank=True)
     solicitudesPendientes = models.ManyToManyField('self', blank=True)
 
@@ -21,14 +22,11 @@ class PersonaNatural(models.Model):
             raise ValueError('Usuarios deben tener un email.')
         if not password:
             raise ValueError('Usuarios deben tener una contraseña.')
-
         user = User.objects.create_user(first_name=primerNombre, last_name=apellido, email=email, username=email,
                                         password=password)
         user.save()
-
         persona_natural = PersonaNatural(user=user)
         persona_natural.save()
-
         return persona_natural
 
     def get_nombre(self):
@@ -42,6 +40,12 @@ class PersonaNatural(models.Model):
         # self.user.get_username()
         return self.user.email
 
+    def get_contrasena(self):
+        return self.user.password
+
+    def __str__(self):
+        return f'{self.user.username} PersonaNatural'
+
 
 class Administrador(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -50,17 +54,17 @@ class Administrador(models.Model):
         """
         Creates and saves a superuser with the given email and password.
         """
-
         user = User.objects.create_superuser(email=email, username=email, password=password)
         user.save()
-
         admin = Administrador(user=user)
         admin.save()
-
         return admin
 
     def get_email(self):
         return self.user.email
+
+    def get_password(self):
+        return self.user.password
 
 
 class IActividad(models.Model):
@@ -71,7 +75,6 @@ class IActividad(models.Model):
     def create_i_actividad(self, nombre, descripcion, categoria):
         actividad = IActividad(nombre=nombre, descripcion=descripcion, categoria=categoria)
         actividad.save()
-
         return actividad
 
     def get_nombre(self):
@@ -92,7 +95,6 @@ class ActividadTipo(IActividad):
     def create_actividad_tipo(self, nombre, descripcion, categoria):
         actividad_tipo = super().create_i_actividad(nombre, descripcion, categoria)
         actividad_tipo.save()
-
         return actividad_tipo
 
     def set_fecha_y_hora(self, fecha_y_hora):
@@ -125,7 +127,6 @@ class Actividad(IActividad):
         actividad.persona = persona
         actividad.tiempo = tiempo
         actividad.save()
-
         return actividad
 
     def get_fecha_y_hora(self):
--- BTT4App/urls.py
+++ BTT4App/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', landingPage, name='landingPage'),
-    path('home/', landingPage, name='landingPage'),
-    path('profile/', profile, name='profile'),
-    path('login/', login, name='login'),
-    path('logout/', logout, name='logout'),
-    path('accounts/', include('django.contrib.auth.urls')),
-]
+                  path('', landingPage, name='landingPage'),
+                  path('home/', landingPage, name='landingPage'),
+                  path('profile/', profile, name='profile'),
+                  path('login/', login, name='login'),
+                  path('logout/', logout, name='logout'),
+                  path('accounts/', include('django.contrib.auth.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
